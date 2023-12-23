675531cebbceb9d6d95012466b3ce6dbc51ee289
Pedro Salvador <lp.salvador@info.uas.edu.mx>
2023-12-23 12:02:53 -0700

    uso del useContext

---
+++ src/09-useContext/MainApp.jsx
@@ -0,0 +1,34 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { UserProvider } from "./context/UserProvider"
+import { HomePage } from "./HomePage"
+import { LoginPage } from "./LoginPage"
+import { AboutPage } from "./AboutPage"
+import { Navbar } from "./Navbar"
+
+export const MainApp = () => {
+  return (
+    <UserProvider>
+        
+        {/* <h1>MainApp</h1> */}
+
+        <Navbar />
+
+        <hr />
+        
+        <Routes>
+
+            <Route path="/" element={ <HomePage /> } />
+            <Route path="about" element={ <AboutPage /> } />
+            <Route path="login" element={ <LoginPage /> } />
+
+            
+            {/* <Route path="/*" element={ <LoginPage /> } /> */}
+            <Route path="/*" element={ <Navigate to="/about" /> } />
+        
+        </Routes>
+
+
+
+    </UserProvider>
+  )
+}
